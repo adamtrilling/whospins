df949fd004885b4452e26b4f3b37dcb462e85253
Adam Trilling <adamtrilling@gmail.com>
2013-02-28 23:29:56 -0500

    new rails4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Spinmap::Application.config.secret_key_base = '8b3a88facfaa1c6554d52df20b22d3a1bc88f5fd48a8529cb91ca6df5bf0927de15c0d57fb2c52d3d00ab48bc82cefd54bbdb3102d7ddf99ba864591de11618a'
