c3699bd1c71fe161659215ccba29f66a0c7568f5
Adam Trilling <adamtrilling@gmail.com>
2013-04-15 20:50:47 -0400

    don't need to symlink database.yml, its in version control

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,6 @@ namespace :deploy do
 
   desc "Symlink shared configs and folders on each release."
   task :symlink_shared do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/config/keys.yml #{release_path}/config/keys.yml"
   end
 end
