bef0fde360a700dc8eefa5d2de0bd82454495990
Adam Trilling <adamtrilling@gmail.com>
2013-04-17 20:25:49 -0400

    don't put unix domain socks in /var/run

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
 pidfile "/var/www/whospins/tmp/puma/pid"
 state_path "/var/www/whospins/tmp/puma/state"
-bind 'unix:///var/run/whospins.sock'
+bind 'unix:///var/www/whospins/tmp/whospins.sock'
 activate_control_app
