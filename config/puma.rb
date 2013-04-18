f771bc6eb5b624952fac2ca75eb93f6031a53125
Adam Trilling <adamtrilling@gmail.com>
2013-04-17 20:26:53 -0400

    cap-compliant paths

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
-pidfile "/var/www/whospins/tmp/puma/pid"
-state_path "/var/www/whospins/tmp/puma/state"
-bind 'unix:///var/www/whospins/tmp/whospins.sock'
+pidfile "/var/www/whospins/current/tmp/puma/pid"
+state_path "/var/www/whospins/current/tmp/puma/state"
+bind 'unix:///var/www/whospins/current/tmp/whospins.sock'
 activate_control_app
