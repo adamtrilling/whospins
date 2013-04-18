0032f96f13f5914565ee3762e1b795cd8bb6184e
Adam Trilling <adamtrilling@gmail.com>
2013-04-17 20:35:20 -0400

    try different puma paths

--- config/puma.rb
+++ config/puma.rb
@@ -1,4 +1,4 @@
-pidfile "/var/www/whospins/current/tmp/puma/pid"
-state_path "/var/www/whospins/current/tmp/puma/state"
-bind 'unix:///var/www/whospins/current/tmp/whospins.sock'
+pidfile "/var/www/whospins/current/tmp/puma.pid"
+state_path "/var/www/whospins/shared/sockets/puma.state"
+bind 'unix:///var/www/whospins/shared/sockets/puma.sock'
 activate_control_app
