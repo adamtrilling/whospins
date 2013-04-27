ee3bd28dd8dd196f846afb2fa5843ada138a5e42
Adam Trilling <adamtrilling@gmail.com>
2013-04-26 20:39:00 -0400

    rm duplicate entries in locations_users

---
+++ db/migrate/20130427001230_enforce_user_location_uniqueness.rb
@@ -0,0 +1,16 @@
+class EnforceUserLocationUniqueness < ActiveRecord::Migration
+  def change
+    User.all.each do |u|
+      puts "user #{u.id} #{u.uid}"
+      ids = u.location_ids.uniq
+      u.connection.execute("DELETE FROM locations_users WHERE user_id = #{u.id}")
+      ids.each do |l|
+        u.connection.execute("INSERT INTO locations_users(user_id, location_id) VALUES(#{u.id}, #{l})")
+      end
+    end
+
+    User.last.set_location_numbers
+
+    add_index :locations_users, [:location_id, :user_id], :unique => true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130423234942) do
+ActiveRecord::Schema.define(version: 20130427001230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20130423234942) do
     t.integer "user_id"
   end
 
+  add_index "locations_users", ["location_id", "user_id"], :name => "index_locations_users_on_location_id_and_user_id", :unique => true
   add_index "locations_users", ["location_id"], :name => "index_locations_users_on_location_id"
   add_index "locations_users", ["user_id"], :name => "index_locations_users_on_user_id"
 
