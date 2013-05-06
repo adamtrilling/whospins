5eca7406946b3221bea3e5333509fd3df0cb0ad2
Adam Trilling <adamtrilling@gmail.com>
2013-05-05 22:22:08 -0400

    rm debugging code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,13 +5,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    Rails.logger.info("*** in create action")
     begin
       auth = request.env["omniauth.auth"]
-      Rails.logger.info("*** authenticating with #{auth}")
       user = User.where(:provider => auth['provider'], 
                         :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
-      Rails.logger.info("*** user = #{user.id}")
       session[:user_id] = user.id
       redirect_to root_url, :notice => 'Signed in!'
     rescue Exception => e
